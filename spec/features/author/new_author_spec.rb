cdc03bd0a9e4615ec4300f7f5fff573f5d8f0712
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-11-09 11:08:08 +0100

    Issue #5: Create test for new author submit

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe "New author page", type: :feature do
+  first_name = 'Alan'
+  last_name = 'Turing'
+  homepage = 'http://wikipedia.org/Alan_Turing'
+
   it "should exist at 'new_author_path' and render without error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
@@ -12,5 +16,16 @@ describe "New author page", type: :feature do
     expect(page).to have_field(name: 'author[first_name]')
     expect(page).to have_field(name: 'author[last_name]')
     expect(page).to have_field(name: 'author[homepage]')
+  end
+
+  it "should create a new author on submit" do
+    visit new_author_path
+    fill_in 'author[first_name]', with: first_name
+    fill_in 'author[last_name]', with: last_name
+    fill_in 'author[homepage]', with: homepage
+
+    find('input[type="submit"]').click
+
+    expect(Author.exists?(first_name: first_name, last_name: last_name, homepage: homepage)).to be_truthy
   end
 end
