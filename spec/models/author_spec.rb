de6d358b3093d12695ac209cf185d9021828eac9
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-11-09 10:39:54 +0100

    Use author factory in tests
    Found out about factories und set up a author factory with the Alan Turing Data

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe Author, type: :model do
   it "should have attributes for the author's first name, last_name and homepage" do
-    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+    author = create(:author)
     expect(author.first_name).to eq 'Alan'
     expect(author.last_name).to eq 'Turing'
     expect(author.homepage).to eq 'http://wikipedia.org/Alan_Turing'
   end
 
   it "should have a name method that combines the first name and the last name" do
-    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+    author = create(:author)
     expect(author.name).to eq 'Alan Turing'
   end
 end
