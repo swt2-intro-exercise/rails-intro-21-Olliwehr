8ab43946aa2a975d9a88f4d776e9edb4bdf71a64
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-11-09 11:19:53 +0100

    Issue #5: Save author from new authors page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,6 +3,14 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    render plain: params[:author].inspect
+    @author = Author.new(author_parameters)
+
+    @author.save
+    redirect_to root_path, notice: 'Success!'
   end
+
+  private
+    def author_parameters
+      params.require(:author).permit(:first_name, :last_name, :homepage)
+    end
 end
