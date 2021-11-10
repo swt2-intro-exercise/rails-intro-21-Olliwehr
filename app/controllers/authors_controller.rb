10f2b5ec98a280320f1f02afa7c809cd9549f92d
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-11-10 16:41:15 +0100

    Issue #11: Show link to delete an author on the authors index page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -35,6 +35,13 @@ class AuthorsController < ApplicationController
     end
   end
 
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+
+    redirect_to authors_path
+  end
+
   private
     def author_parameters
       params.require(:author).permit(:first_name, :last_name, :homepage)
