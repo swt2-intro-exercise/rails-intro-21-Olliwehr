3f3900e48a76d4e82e3c864dadd45ab032bccb00
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-11-12 15:16:55 +0100

    Issue #17: papers index page -> make year specifiable via url param

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -5,4 +5,10 @@ class Paper < ApplicationRecord
   validates :year, numericality: true
 
   has_and_belongs_to_many :authors
+
+  scope :year, ->(year) { year_eq(year) if year.present? }
+
+  def self.year_eq(year)
+    where("year = ?", year)
+  end
 end
