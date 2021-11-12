70022de078382c3ae6a692ee58bdeafe6e332644
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-11-12 13:50:05 +0100

    Issue #14: Create n:m relationship between authors and papers

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,6 @@ class Paper < ApplicationRecord
   validates :venue, presence: true
   validates :year, presence: true
   validates :year, numericality: true
+
+  has_and_belongs_to_many :authors
 end
